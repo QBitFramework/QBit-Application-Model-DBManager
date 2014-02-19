e10bcc65bda14a4ed9a7011e746fc1ebfc51846e
Dmitry Lukiyanchuk <tertius@yandex-team.ru>
2014-02-19 12:33:11 +0400

    Add multistate_groups option to DBManager and multistate filter.

--- lib/QBit/Application/Model/DBManager/Filter/multistate.pm
+++ lib/QBit/Application/Model/DBManager/Filter/multistate.pm
@@ -12,10 +12,26 @@ sub pre_process {
     throw gettext('Class "%s" must be descedant of "QBit::Application::Model::Multistate"', ref($self->{'db_manager'}))
       unless $self->{'db_manager'}->isa('QBit::Application::Model::Multistate');
 
-    $field->{'values'} = {
-        map {$_->[0] => ref($_->[1]) eq 'CODE' ? $_->[1]() : $_->[1]}
-        grep {!$_->[2]{'private'}} @{$self->{'db_manager'}->get_multistates_bits()}
-    };
+    my @multistate_bits = grep {!$_->[2]{'private'}} @{$self->{'db_manager'}->get_multistates_bits()};
+
+    if (exists($opts{multistate_groups}) && exists($opts{multistate_groups}{ref($self->{'db_manager'})})) {
+        my @multistate_groups = @{$opts{multistate_groups}{ref($self->{'db_manager'})}};
+
+        if (in_array('__EMPTY__', \@multistate_groups)) {
+            @multistate_bits = map {clone($_)} @multistate_bits;
+
+            $_->[2]{'multistate_groups'} //= ['__EMPTY__'] foreach @multistate_bits;
+        }
+
+        @multistate_bits = grep {
+            exists($_->[2]{'multistate_groups'})
+              && @{arrays_intersection($_->[2]{'multistate_groups'}, \@multistate_groups)}
+        } @multistate_bits;
+    }
+
+    $field->{'values'} = {map {$_->[0] => ref($_->[1]) eq 'CODE' ? $_->[1]() : $_->[1]} @multistate_bits};
+
+    return $field->{'values'};
 }
 
 sub tokens {
@@ -45,12 +61,12 @@ sub expressions {
     my $ns = lc(join('___', @{$opts{'ns'} || []}));
 
     return [
-        $uc_field_name
+        $uc_field_name 
           . " '='  "
           . ($ns ? "${ns}___" : '')
           . "${field_name}___multistate"
           . " { [$_[1] => '='  => \$_[3]] }",
-        $uc_field_name
+        $uc_field_name 
           . " '<>' "
           . ($ns ? "${ns}___" : '')
           . "${field_name}___multistate"
